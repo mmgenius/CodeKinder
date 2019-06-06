278fcd0b8e32b87258b1838e6f7d1daee4e6e0e4
idrissa.seydou-issaka <idrissa.seydou-issaka@mathinfo.i-univ-tlse2.fr>
2019-06-06 17:32:55 +0200

    Documentation

--- API_MODEL_DSL/src-gen/APIExchange/EMFModelLoad.java
+++ API_MODEL_DSL/src-gen/APIExchange/EMFModelLoad.java
@@ -15,51 +15,68 @@ import model.ModelPackage;
 import model.Period;
 import model.Query;
 
-
+/**
+ * @author Idrissa SI
+ * This class allows to load a created instance of Query model and BlockOfResult  
+ * 
+ * */
 public class EMFModelLoad {
-    public BlockOfResult load() {
-        // Initialize the model
-    	ModelPackage.eINSTANCE.eClass();
 
-        // Register the XMI resource factory for the .query extension
 
-        Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-        Map<String, Object> m = reg.getExtensionToFactoryMap();
-        m.put("result", new XMIResourceFactoryImpl());
+	/**
+	 * @author Idrissa SI
+	 * @return {@link BlockOfResult}
+	 * load created BlockOfResult model instance
+	 * */
+	public BlockOfResult load() {
+		// Initialize the model
+		ModelPackage.eINSTANCE.eClass();
 
-        // Obtain a new resource set
-        ResourceSet resSet = new ResourceSetImpl();
+		// Register the XMI resource factory for the .query extension
 
-        // Get the resource
-        Resource resource = resSet.getResource(URI
-                .createURI("../API_MODEL_DSL/result/My2.result"), true);
-        // Get the first model element and cast it to the right type, in my
-        // example everything is hierarchical included in this first node
-        BlockOfResult result = (BlockOfResult) resource.getContents().get(0);
-        return result;
-    }
-    
-    public Query loadQuery() {
-        // Initialize the model
-    	ModelPackage.eINSTANCE.eClass();
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("result", new XMIResourceFactoryImpl());
 
-        // Register the XMI resource factory for the .website extension
+		// Obtain a new resource set
+		ResourceSet resSet = new ResourceSetImpl();
 
-        Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-        Map<String, Object> m = reg.getExtensionToFactoryMap();
-        m.put("query", new XMIResourceFactoryImpl());
+		// Get the resource
+		Resource resource = resSet.getResource(URI
+				.createURI("../API_MODEL_DSL/result/My2.result"), true);
+		// Get the first model element and cast it to the right type, in my
+		// example everything is hierarchical included in this first node
+		BlockOfResult result = (BlockOfResult) resource.getContents().get(0);
+		return result;
+	}
 
-        // Obtain a new resource set
-        ResourceSet resSet = new ResourceSetImpl();
+	
+	/**
+	 * @author Idrissa SI
+	 * @return {@link Query}
+	 * load created Query model instance
+	 * */
+	public Query loadQuery() {
+		// Initialize the model
+		ModelPackage.eINSTANCE.eClass();
 
-        // Get the resource
-        Resource resource = resSet.getResource(URI.createURI("../API_MODEL_DSL/query/requete.query"), true);
-        // Get the first model element and cast it to the right type, in my
-        // example everything is hierarchical included in this first node
-   ;
-        Query query =  (Query) resource.getContents().get(0);
-        System.out.println("xcxcxc" +query);
-        return query;
-    }
+		// Register the XMI resource factory for the .website extension
+
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("query", new XMIResourceFactoryImpl());
+
+		// Obtain a new resource set
+		ResourceSet resSet = new ResourceSetImpl();
+
+		// Get the resource
+		Resource resource = resSet.getResource(URI.createURI("../API_MODEL_DSL/query/requete.query"), true);
+		// Get the first model element and cast it to the right type, in my
+		// example everything is hierarchical included in this first node
+
+		Query query =  (Query) resource.getContents().get(0);
+		System.out.println("xcxcxc" +query);
+		return query;
+	}
 
 }
